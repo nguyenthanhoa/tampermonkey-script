6340559e0975296d2be229fa34f596d227d7116c
nguyenthanhoa <41941451+nguyenthanhoa@users.noreply.github.com>
2026-04-17 11:08:23 +0700

    Add files via upload

---
+++ off-block-internet.js
@@ -0,0 +1,54 @@
+// ==UserScript==
+// @name         Auto off connect block internet
+// @namespace    http://tampermonkey.net/
+// @version      2026-04-17
+// @description  try to take over the world!
+// @author       You
+// @match        https://localhost:40001/ui/main/connection
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=undefined.localhost
+// @grant        none
+// ==/UserScript==
+
+
+(function() {
+    'use strict';
+    // 2. Hàm xử lý chính
+
+    function processElements() {
+        // Tìm tất cả các div có attribute data-v-55b02123
+        const attr ='data-v-55b02123'
+        const allPotentialParents = document.querySelectorAll(`div[${attr}].item-app`);
+        if(!allPotentialParents)return;
+        allPotentialParents.forEach(parent => {
+            if (parent) {
+                // Nếu thỏa mãn điều kiện cha-con, tìm tiếp icon
+                const icon = parent.querySelector(`i[${attr}].icon-block-network-2`);
+
+                if (icon) {
+                    const wrapper = parent.parentElement;
+                    if (wrapper) {
+                        // Tìm thẻ input có aria-checked="true"
+                        const input = wrapper.querySelector('input[aria-checked="true"]');
+
+                        if (input) {
+                            // Lấy text từ thẻ font-weight-semi-bold
+                            const labelDiv = parent.querySelector(`div[${attr}].font-weight-semi-bold`);
+                            const text = labelDiv ? labelDiv.innerText.trim() : "Không rõ tên";
+
+                            // Hiển thị thông báo tự đóng
+                            console.log('Ngắt kết nối đến: ' + text);
+
+                            // Click vào input
+                            input.click();
+                        }}
+                }
+            }
+        });
+    }
+
+
+
+    // Chạy lần đầu
+    //processElements();
+    setInterval(processElements, 5000);
+})();
