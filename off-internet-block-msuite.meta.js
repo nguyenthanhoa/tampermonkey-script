bfef16975a97191c330ba032434421c668f9cf27
nguyenthanhoa <41941451+nguyenthanhoa@users.noreply.github.com>
2026-04-23 08:08:21 +0700

    Add metadata for Auto off internet block connect script

---
+++ off-internet-block-msuite.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Auto off internet block connect
+// @namespace    http://tampermonkey.net/
+// @downloadURL  https://github.com/nguyenthanhoa/tampermonkey-script/raw/refs/heads/main/off-internet-block-msuite.user.js
+// @updateURL    https://github.com/nguyenthanhoa/tampermonkey-script/raw/refs/heads/main/off-internet-block-msuite.meta.js
+// @version      2026-04-21
+// @description  try to take over the world!
+// @author       You
+// @match        https://localhost:40001/ui/main/connection
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=undefined.localhost
+// @grant        none
+// ==/UserScript==
